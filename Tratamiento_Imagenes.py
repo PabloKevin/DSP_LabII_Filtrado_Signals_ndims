8c51aacd351a3992f8dfb3f1f3d86cd03e2bbc26
PabloKevin <pablo.pereira.p@estudiantes.utec.edu.uy>
2025-11-10 21:50:10 -0300

    pasar el .py

---
+++ Tratamiento_Imagenes.py
@@ -0,0 +1,201 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+cv2.__version__
+
+img = cv2.imread("DSP_LabII_Filtrado_Signals_ndims/images/sun.jpg", 1)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+gray_img = rgb2gray(cv2.imread("DSP_LabII_Filtrado_Signals_ndims/images/sun.jpg",1))
+
+IMG = np.fft.fft2(gray_img)
+IMG = np.fft.fftshift(IMG)
+magnitude_spectrum = 20*np.log(np.abs(IMG))
+
+sigma_x, sigma_y = 50, 50 # 26, 26
+(ncols, nrows) = np.shape(gray_img)
+c_x, c_y = nrows/2, ncols/2
+x = np.linspace(0, nrows, nrows)
+y = np.linspace(0, ncols, ncols)
+X, Y = np.meshgrid(x, y)
+gaussian_mask = np.exp(-((X-c_x)**2 + (Y-c_y)**2)/(2*sigma_x**2)) / (2*np.pi*sigma_x**2)
+
+filtered_IMG = IMG * gaussian_mask
+
+ifft_img = np.fft.ifft2(filtered_IMG)
+
+fig, axes = plt.subplots(3, 2, figsize=(8, 6))  
+
+axes[0][0].imshow(img,cmap='gray')
+axes[0][0].set_title('Imagen original RGB')
+axes[0][0].axis('off') 
+
+axes[0][1].imshow(gray_img,cmap='gray')
+axes[0][1].set_title('Imagen en escala de grises')
+axes[0][1].axis('off') 
+
+axes[1][0].imshow(magnitude_spectrum, cmap = 'magma') 
+axes[1][0].set_title('Magnitud del especto gray')
+axes[1][0].axis('off') 
+
+axes[1][1].imshow(gaussian_mask, cmap = 'gray')
+axes[1][1].set_title('Máscara Gaussiana')
+axes[1][1].axis('off') 
+
+axes[2][0].imshow(20*np.log(np.abs(filtered_IMG)), cmap = 'magma')
+axes[2][0].set_title('Espectro filtrado')
+axes[2][0].axis('off') 
+
+axes[2][1].imshow(np.abs(ifft_img), cmap = 'gray')
+axes[2][1].set_title('Imagen filtrada')
+axes[2][1].axis('off') 
+
+plt.tight_layout() 
+plt.show()
+
+# con cv2
+gauss_img = cv2.GaussianBlur(gray_img, (7, 7), 0)
+
+
+# Detección de bordes
+gray_u8 = gray_img.astype(np.uint8)
+bordes = cv2.Canny(gray_u8, 110, 250, 15)
+
+fig, axes = plt.subplots(3, 1, figsize=(6, 8))  
+gray_u8 = gray_img.astype(np.uint8)
+sobelx = cv2.Sobel(gray_u8, cv2.CV_64F, 1, 0, ksize=15)
+sobely = cv2.Sobel(gray_u8, cv2.CV_64F, 0, 1, ksize=15)
+sobel = cv2.magnitude(sobelx, sobely)
+axes[0].imshow(sobelx, cmap='gray')
+axes[0].set_title('Filtro Sobel X')
+axes[0].axis('off') 
+axes[1].imshow(sobely, cmap='gray')
+axes[1].set_title('Filtro Sobel Y')
+axes[1].axis('off') 
+axes[2].imshow(sobel, cmap='gray')
+axes[2].set_title('Filtro Sobel magnitud')
+axes[2].axis('off') 
+plt.show()
+
+fig, axes = plt.subplots(3, 1, figsize=(6, 8))  
+
+axes[0].imshow(gray_img, cmap='gray')
+axes[0].set_title('Imagen original en escala de grises')
+axes[0].axis('off') 
+axes[1].imshow(bordes, cmap='gray')
+axes[1].set_title('Filtro Canny para detección de bordes')
+axes[1].axis('off') 
+axes[2].imshow(sobel, cmap='gray')
+axes[2].set_title('Filtro Sobel magnitud para detección de bordes')
+axes[2].axis('off') 
+plt.show()
+
+gauss_img = cv2.GaussianBlur(gray_img, (3, 3), 0)
+in_img = gauss_img.astype(np.uint8)
+circles = cv2.HoughCircles(in_img, cv2.HOUGH_GRADIENT, 2.5, 500, minRadius=200,maxRadius=320)
+print("cantidad de circulos encontrados:", circles.shape[1] if circles is not None else 0)
+cimg = cv2.cvtColor(in_img, cv2.COLOR_GRAY2BGR)
+if circles is not None:
+    circles = np.uint16(np.around(circles)) # redefinir tipo de datos
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(155,200,50),12) # dibujar los circulos identificados en verde (BGR)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),5,(255,0,0),15)# dibujar los centros de circulos identificados en rojo (BGR)
+
+o_img = img.astype(np.uint8)
+
+print(np.shape(in_img))
+if circles is not None:
+    circles = np.uint16(np.around(circles)) # redefinir tipo de datos
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(o_img,(i[0],i[1]),i[2],(0,0,255),12) # dibujar los circulos identificados en verde (BGR)
+        # draw the center of the circle
+        cv2.circle(o_img,(i[0],i[1]),5,(0,0,255),15) # dibujar los centros de circulos identificados en rojo (BGR)
+
+# Crear figura y ejes (2 filas, 1 columna)
+fig, axes = plt.subplots(2, 1, figsize=(6, 5))  # 2 filas, 1 columna
+
+# Primer subplot (arriba)
+axes[0].imshow(cimg)
+axes[0].set_title('Imagen gray con círculos detectados')
+axes[0].axis('off')
+
+# Segundo subplot (abajo)
+axes[1].imshow(o_img)
+axes[1].set_title('Círuclos detectados plotteados sobre la imagen original')
+axes[1].axis('off')
+
+plt.tight_layout()  # Ajusta espacios para que no se sobrepongan
+plt.show()
+
+
+cmaps = ['Reds', 'Greens', 'Blues']
+h, w = in_img.shape
+
+
+fig, axes = plt.subplots(4, 1, figsize=(6, 8))  # 2 filas, 1 columna
+axes[0].imshow(img)
+axes[0].set_title('Imagen original RGB')
+axes[0].axis('off')
+
+for channel in range(3):
+    rgb_img = np.zeros((h, w, 3), dtype=np.uint8)
+    rgb_img[..., channel] = img[...,channel]  # verde
+    axes[channel+1].imshow(rgb_img, cmap=cmaps[channel])
+    axes[channel+1].set_title('Canal '+ cmaps[channel])
+    axes[channel+1].axis('off')
+
+plt.tight_layout()  # Ajusta espacios para que no se sobrepongan
+plt.show()
+
+# Approach por canales 
+
+in_img = img[...,1].astype(np.uint8)
+in_img = cv2.GaussianBlur(in_img, (5, 5), 0)
+circles = cv2.HoughCircles(in_img, cv2.HOUGH_GRADIENT, 2.0, 600, minRadius=200,maxRadius=320)
+print("cantidad de circulos encontrados:", circles.shape[1] if circles is not None else 0)
+
+h, w = in_img.shape
+rgb_img = np.zeros((h, w, 3), dtype=np.uint8)
+rgb_img[..., 1] = in_img  # verde
+
+print(np.shape(in_img))
+if circles is not None:
+    circles = np.uint16(np.around(circles)) # redefinir tipo de datos
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(rgb_img,(i[0],i[1]),i[2],(0,0,255),12) # dibujar los circulos identificados en verde (BGR)
+        # draw the center of the circle
+        cv2.circle(rgb_img,(i[0],i[1]),5,(255,0,0),15) # dibujar los centros de circulos identificados en rojo (BGR)
+
+o_img = img.astype(np.uint8)
+
+print(np.shape(in_img))
+if circles is not None:
+    circles = np.uint16(np.around(circles)) # redefinir tipo de datos
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(o_img,(i[0],i[1]),i[2],(0,0,255),12) # dibujar los circulos identificados en verde (BGR)
+        # draw the center of the circle
+        cv2.circle(o_img,(i[0],i[1]),5,(0,0,255),15) # dibujar los centros de circulos identificados en rojo (BGR)
+
+# Crear figura y ejes (2 filas, 1 columna)
+fig, axes = plt.subplots(2, 1, figsize=(6, 5))  # 2 filas, 1 columna
+
+# Primer subplot (arriba)
+axes[0].imshow(rgb_img,cmap='Greens')
+axes[0].set_title('Canal verdes con círculos detectados')
+axes[0].axis('off')
+
+# Segundo subplot (abajo)
+axes[1].imshow(o_img)
+axes[1].set_title('Círuclos detectados plotteados sobre la imagen original')
+axes[1].axis('off')
+
+plt.tight_layout()  # Ajusta espacios para que no se sobrepongan
+plt.show()
